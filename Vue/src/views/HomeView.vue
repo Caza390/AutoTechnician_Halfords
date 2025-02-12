38cc486847a0141d847c6eeb3c08e53f99d973b6
Caz <callumcb1@hotmail.co.uk>
2025-02-12 02:21:50 +0000

    Adding a basic front page

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -2,9 +2,28 @@
 </script>
 
 <template>
+  <main class="flex justify-center items-center p-20">
+    <div class="grid grid-cols-3 gap-20">
+      <div class="bg-orange-500 w-72 h-72 p-6 rounded-lg flex flex-col items-center">
+        <div class="w-56 h-36 bg-orange-400 border border-black flex items-center justify-center">
+          <span>Img</span>
+        </div>
+        <p class="mt-4 font-bold text-black">Dash Camera</p>
+      </div>
 
-  <main class="p-5 md:px-40 md:py-20">
-    <p>Homepage</p>
+      <div class="bg-orange-500 w-72 h-72 p-6 rounded-lg flex flex-col items-center">
+        <div class="w-56 h-36 bg-orange-400 border border-black flex items-center justify-center">
+          <span>Img</span>
+        </div>
+        <p class="mt-4 font-bold text-black">Audio</p>
+      </div>
+
+      <div class="bg-orange-500 w-72 h-72 p-6 rounded-lg flex flex-col items-center">
+        <div class="w-56 h-36 bg-orange-400 border border-black flex items-center justify-center">
+          <span>Img</span>
+        </div>
+        <p class="mt-4 font-bold text-black">Speakers</p>
+      </div>
+    </div>
   </main>
-
 </template>
