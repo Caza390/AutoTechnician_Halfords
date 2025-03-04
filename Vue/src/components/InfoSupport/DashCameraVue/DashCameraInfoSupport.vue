563164f9788f7f11aa4bbadda375f816bb34bac2
Caz <callumcb1@hotmail.co.uk>
2025-03-04 22:56:06 +0000

    Changes to the site along with added data

--- Vue/src/components/InfoSupport/DashCameraVue/DashCameraInfoSupport.vue
+++ Vue/src/components/InfoSupport/DashCameraVue/DashCameraInfoSupport.vue
@@ -4,31 +4,34 @@
 <template>
   <main class="flex justify-center items-center p-20">
     <div class="absolute top-8 right-6">
-    <RouterLink :to="`/`" class="bg-red-500 px-4 py-2 rounded-lg shadow-md hover:bg-red-700">
-      Return
-    </RouterLink>
-  </div>
+      <RouterLink :to="`/`" class="bg-red-500 px-4 py-2 rounded-lg shadow-md hover:bg-red-700">
+        Return
+      </RouterLink>
+    </div>
 
     <div class="grid grid-cols-3 gap-20">
-      <RouterLink :to="'/dash-camera-info-support/cameras-information'" class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
+      <RouterLink :to="'/dash-camera-info-support/camera-information'"
+        class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
         <div class="w-56 h-36 bg-halfords-orange-300 border border-black flex items-center justify-center">
-          <img src="/Images/Models/Insignia.png" alt="Vehicle selection interface" class="w-full h-full object-cover" />
+          <img src="/Images/Models/Insignia.png" alt="information" class="w-full h-full object-contain" />
         </div>
-        <p class="mt-4 font-bold text-black">Cameras Information</p>
+        <p class="mt-3 font-bold text-black text-3xl text-center">Dash Camera Information</p>
       </RouterLink>
 
-      <RouterLink :to="'/dash-camera-info-support/how-to'" class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
+      <RouterLink :to="'/dash-camera-info-support/how-to'"
+        class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
         <div class="w-56 h-36 bg-halfords-orange-300 border border-black flex items-center justify-center">
-          <img src="/Images/Models/Insignia.png" alt="Car audio system" class="w-full h-full object-cover" />
+          <img src="/Images/Models/Insignia.png" alt="how-to" class="w-full h-full object-contain" />
         </div>
-        <p class="mt-4 font-bold text-black">How To</p>
+        <p class="mt-3 font-bold text-black text-3xl text-center">How To</p>
       </RouterLink>
 
-      <RouterLink :to="'/dash-camera-info-support/troubleshooting'" class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
+      <RouterLink :to="'/dash-camera-info-support/troubleshooting'"
+        class="bg-halfords-orange-400 w-72 h-72 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
         <div class="w-56 h-36 bg-halfords-orange-300 border border-black flex items-center justify-center">
-          <img src="/Images/Models/Insignia.png" alt="High-quality car speakers" class="w-full h-full object-cover" />
+          <img src="/Images/Models/Insignia.png" alt="troubleshoot" class="w-full h-full object-contain" />
         </div>
-        <p class="mt-4 font-bold text-black">Trouble shooting</p>
+        <p class="mt-3 font-bold text-black text-3xl text-center">Troubleshooting</p>
       </RouterLink>
     </div>
   </main>
--- Vue/src/components/MakeView.vue
+++ Vue/src/components/MakeView.vue
@@ -15,7 +15,6 @@ const loadMakes = async () => {
   }
 };
 
-// Watch for changes in the route
 watch(() => route.params.category, (newCategory) => {
   category.value = newCategory;
   loadMakes();
@@ -31,22 +30,15 @@ onMounted(loadMakes);
     </RouterLink>
   </div>
 
-  <main class="flex flex-col items-center p-10">
-    <!-- Category Makes -->
-    <div v-if="makes.length > 0" class="grid grid-cols-4 gap-8">
-      <RouterLink 
-        v-for="make in makes" 
-        :key="make.id" 
-        :to="`/vehicle-select/${make.Make.toLowerCase()}`"
-        class="bg-halfords-orange-400 w-64 h-64 p-4 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
-        <img :src="make.Image" :alt="make.Make" class="w-28 h-28 py-4 object-contain" />
-        <p class="mt-3 font-bold text-black text-3xl">{{ make.Make }}</p>
+  <main class="flex justify-center items-center p-20">
+    <div class="grid grid-cols-4 gap-8">
+      <RouterLink v-for="make in makes" :key="make.id" :to="`/vehicle-select/${make.Make.toLowerCase()}`"
+      class="bg-halfords-orange-400 w-72 h-72 p-4 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500">
+        <div class="w-56 h-36 bg-halfords-orange-300 border border-black flex items-center justify-center">
+          <img :src="make.Image" :alt="make.Make" class="w-full h-full object-contain" />
+        </div>
+        <p class="mt-3 font-bold text-black text-3xl text-center">{{ make.Make }}</p>
       </RouterLink>
-    </div>
-
-    <div v-else>
-      <p class="text-red-500 text-lg mb-4">No Data Provided</p>
-      <RouterLink to="/" class="border-4 p-1">Return</RouterLink>
     </div>
   </main>
 </template>
