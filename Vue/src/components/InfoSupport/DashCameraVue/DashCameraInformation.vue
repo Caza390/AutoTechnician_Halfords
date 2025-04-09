3c8f3a503f8e698f9193e77e13d60c8cdeee37f0
Caz <callumcb1@hotmail.co.uk>
2025-04-09 23:47:02 +0100

    Updated Layout and Added BlackVue Cameras to lists

--- Vue/src/components/InfoSupport/DashCameraVue/DashCameraInformation.vue
+++ Vue/src/components/InfoSupport/DashCameraVue/DashCameraInformation.vue
@@ -29,20 +29,29 @@ const closeModal = () => {
 
     <div class="grid grid-cols-4 gap-8">
       <div v-for="(camera) in dashCameras" :key="camera.ArticleId"
-        class="bg-halfords-orange-400 w-80 p-6 rounded-lg flex flex-col items-center hover:bg-halfords-orange-500 shadow-lg">
+        class="bg-halfords-orange-400 w-80 p-6 rounded-lg hover:bg-halfords-orange-500 shadow-lg flex flex-col h-96">
+
         <!-- Image -->
-        <div class="w-64 h-40 bg-white border border-black flex items-center justify-center">
+        <div class="w-64 h-40 bg-white border border-black flex items-center justify-center mx-auto">
           <img :src="camera.Image" :alt="camera.Type" class="w-full h-full object-contain" />
         </div>
 
-        <!-- Details -->
-        <p class="mt-4 font-bold text-black text-lg">{{ camera.Brand }}</p>
-        <p class="font-bold text-black text-lg">{{ camera.Type }}</p>
+        <!-- Text and Button wrapper -->
+        <div class="flex flex-col flex-grow justify-between mt-4">
+          <!-- Text -->
+          <div>
+            <p class="font-bold text-black text-lg text-center">{{ camera.Brand }}</p>
+            <p class="font-bold text-black text-lg text-center">{{ camera.Type }}</p>
+            <p v-if="camera.OnlineExclusive" class="font-bold text-black text-lg text-center">{{ camera.OnlineExclusive
+              }}
+            </p>
+          </div>
 
-        <!-- Open Modal -->
-        <button @click="openModal(camera)" class="mt-4 bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800">Show
-          More Details
-        </button>
+          <!-- Button pinned to bottom -->
+          <button @click="openModal(camera)" class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 mt-4">
+            Show More Details
+          </button>
+        </div>
       </div>
     </div>
   </main>
