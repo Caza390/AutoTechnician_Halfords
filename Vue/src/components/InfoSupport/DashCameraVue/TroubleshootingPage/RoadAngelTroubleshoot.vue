03da3c63aee92b5caf3795368b644bf2797c7e1c
Caz <callumcb1@hotmail.co.uk>
2025-04-13 01:22:24 +0100

    Added category via brand for troubleshooting pages

---
+++ Vue/src/components/InfoSupport/DashCameraVue/TroubleshootingPage/RoadAngelTroubleshoot.vue
@@ -0,0 +1,26 @@
+
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="absolute top-8 right-6">
+        <RouterLink to="/dash-camera-info-support/troubleshooting" class="bg-red-500 px-4 py-2 rounded-lg shadow-md hover:bg-red-700">
+            Return
+        </RouterLink>
+    </div>
+
+    <main class="grid items-center justify-center p-20">
+        <h1 class="text-6xl font-bold bg-black bg-opacity-20 rounded-lg p-2 text-halfords-orange-400">RoadAngel
+            Troubleshooting
+        </h1>
+        <div class="my-2">
+            <h2 class="text-xl font-bold mb-1">Issue 1:</h2>
+            <p>Paragraph 1</p>
+        </div>
+
+        <div class="my-2">
+            <h2 class="text-xl font-bold mb-1">Issue 2:</h2>
+            <p>Paragraph 2</p>
+        </div>
+    </main>
+</template>
